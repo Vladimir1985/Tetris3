66a00674434b0239b6f4a4419c514f6d103ad346
Vladimir O <vladimir_ovdienk@mail.ru>
2017-10-27 09:44:44 +0400

    17.10.2017

--- app/src/main/java/com/example/vladimir/serialize/CurrentStateDeserializer.java
+++ app/src/main/java/com/example/vladimir/serialize/CurrentStateDeserializer.java
@@ -1,7 +1,14 @@
 package com.example.vladimir.serialize;
 
+import com.example.vladimir.struct.CubeObject;
 import com.example.vladimir.struct.CurrentState;
 import com.example.vladimir.struct.GameObject;
+import com.example.vladimir.struct.LAngleObject;
+import com.example.vladimir.struct.LZObject;
+import com.example.vladimir.struct.RAngleObject;
+import com.example.vladimir.struct.RZObject;
+import com.example.vladimir.struct.StraightObject;
+import com.example.vladimir.struct.TObject;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -10,21 +17,41 @@ import com.google.gson.JsonParseException;
 
 import java.lang.reflect.Type;
 
-/**
- * Created by Vladimir on 11.10.2017.
- */
 
 public class CurrentStateDeserializer implements JsonDeserializer<CurrentState> {
 
     public CurrentState deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
     {
-        GameObject gm = null;
-        CurrentState cs=new CurrentState();
-       // String data = json.getAsString();
+
+        CurrentState currentState=new CurrentState();
+
         JsonObject jsonObject = json.getAsJsonObject();
-        cs.SetObject((GameObject) context.deserialize(jsonObject.get("GameObject"), GameObject.class));
-        //gm=((GameObject) context.deserialize(jsonObject.get("GameObject"), GameObject.class));
-        // gm.SetObjectCoord(context.deserialize(data,int[][]));
-        return cs;
+
+        currentState.SetCurrentFigure(jsonObject.get("objecttype").getAsInt());
+
+        int []i=context.deserialize(jsonObject.get("figures"),int[].class);
+        currentState.SetFigures(i);
+
+        boolean [][]coord=context.deserialize(jsonObject.get("battlefield"),boolean[][].class);
+        currentState.SetPanel(coord);
+
+        currentState.SetScore(jsonObject.get("score").getAsInt());
+
+        currentState.SetLevel(jsonObject.get("level").getAsInt());
+
+        switch (currentState.GetCurrentFigure())
+        {
+            case 0:currentState.SetObject((CubeObject) context.deserialize(jsonObject.get("GameObject"), CubeObject.class));break;
+            case 1:currentState.SetObject((StraightObject) context.deserialize(jsonObject.get("GameObject"), StraightObject.class));break;
+            case 2:currentState.SetObject((TObject) context.deserialize(jsonObject.get("GameObject"), TObject.class));break;
+            case 3:currentState.SetObject((RAngleObject) context.deserialize(jsonObject.get("GameObject"), RAngleObject.class));break;
+            case 4:currentState.SetObject((LAngleObject) context.deserialize(jsonObject.get("GameObject"), LAngleObject.class));break;
+            case 5:currentState.SetObject((LZObject) context.deserialize(jsonObject.get("GameObject"), LZObject.class));break;
+            case 6:currentState.SetObject((RZObject) context.deserialize(jsonObject.get("GameObject"), RZObject.class));break;
+
+            default:break;
+        }
+
+        return currentState;
     }
 }
