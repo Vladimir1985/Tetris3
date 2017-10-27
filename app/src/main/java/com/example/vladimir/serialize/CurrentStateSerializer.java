e2bb463cefe309de203789edb292a5c5070a1655
Vladimir O <vladimir_ovdienk@mail.ru>
2017-10-27 09:43:17 +0400

    17.10.2017

--- app/src/main/java/com/example/vladimir/serialize/CurrentStateSerializer.java
+++ app/src/main/java/com/example/vladimir/serialize/CurrentStateSerializer.java
@@ -14,45 +14,14 @@ public class CurrentStateSerializer implements JsonSerializer<CurrentState>
     @Override
     public JsonElement serialize(CurrentState src, Type typeOfSrc, JsonSerializationContext context)
     {
-//        StringBuilder sb=new StringBuilder();
-//        String bf = "";
-//        String coord = "";
-////        //Переносим данные о состоянии игрового поля со всеми фигурами
-//        for(int i=0;i<src.GetPanel()[0].length;i++)
-//            for (int j = 0; j < src.GetPanel().length; j++)
-//            {
-//                if(src.GetPanel()[j][i])
-//                    sb.append('1');
-//                else
-//                    sb.append('0');
-//            }
-//        sb.append(" ");
-//
-//        for(int i=0;i<src.GetObject().GetObjectCoord()[0].length;i++)
-//            for (int j = 0; j < src.GetObject().GetObjectCoord().length; j++)
-//            {
-//                sb.append(src.GetObject().GetObjectCoord()[i][j]);
-//            }
-//
-//        sb.append(" ");
-//
-//        for(int i=0;i<src.countFigures;i++)
-//            sb.append(src.GetFigures()[i]);
-//
-//        sb.append(" ");
-//
-//        sb.append(src.GetScore());
-//
-//        sb.append(" ");
-//        sb.append(src.GetLevel());
-//
-//        return new JsonPrimitive(sb.toString() );
         JsonObject result = new JsonObject();
         result.add("GameObject",context.serialize(src.GetObject()));
         result.add("battlefield",context.serialize(src.GetPanel()));
         result.add("figures",context.serialize(src.GetFigures()));
         result.addProperty("score",src.GetScore());
         result.addProperty("level",src.GetLevel());
+        result.addProperty("objecttype",src.GetCurrentFigure());
+
         return result;
     }
 }
